c6cab2b8df5d67b19db7aed82acc567bc3d2b6a9
Jeff Hykin <jeff.hykin@gmail.com>
2023-09-22 07:59:16 -0500

    revert: caching needs too much ram apparently

--- main/add/basic_features.py
+++ main/add/basic_features.py
@@ -5,11 +5,11 @@ from generic_tools.misc import *
 from specific_tools import *
 import specific_tools
 
-
 # 
 # read data
 # 
 path = path_to.all_sites
+df = pandas.read_csv(path, sep="\t")
 
 FS.ensure_is_file(path_to.all_sites)
 first_line = ""
@@ -18,23 +18,14 @@ with open(path_to.all_sites, "r+") as file:
         first_line = line.strip()
         break
 
-@cache()
-def get_new_rows(path):
-    df = pandas.read_csv(path, sep="\t")
-    rows = []
-    for progress, (site_id, uniprot_gene_id, index_relative_to_gene, amino_acids, is_serine_site, is_threonine_site, is_tyrosine_site, is_human, is_phos_site, *_) in ProgressBar(df.values):
-        rows.append([
-            # converts NaN's to empty strings
-            str(each) if each == each else ""
-                for each in ([ site_id, uniprot_gene_id, index_relative_to_gene, amino_acids, is_serine_site, is_threonine_site, is_tyrosine_site, is_human, is_phos_site ] + specific_tools.amino_window_to_feature_vec(amino_acids).tolist())
-        ])
-    return rows
-    
-
 new_filepath = path_to.all_sites+".new"
 FS.ensure_is_file(new_filepath)
 with open(new_filepath, "r+") as file:
-    new_rows = get_new_rows(path)
-    file.write(first_line + "\t" + ("\t".join(basic_feature_names))+"\n")
-    for each in ProgressBar(rows):
-        file.write("\t".join(each)+"\n")
+    file.write(first_line + ("\t".join(basic_feature_names))+"\n")
+    for progress, (site_id, uniprot_gene_id, index_relative_to_gene, amino_acids, is_serine_site, is_threonine_site, is_tyrosine_site, is_human, is_phos_site, *_) in ProgressBar(df.values):
+        values = [
+            # converts NaN's to empty strings
+            str(each) if each == each else ""
+                for each in ([ site_id, uniprot_gene_id, index_relative_to_gene, amino_acids, is_serine_site, is_threonine_site, is_tyrosine_site, is_human, ] + specific_tools.amino_window_to_feature_vec(amino_acids).tolist())
+        ]
+        file.write("\t".join(values)+"\n")
